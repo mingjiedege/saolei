e91c982477a43fd95af2cc3ce3646954316298c4
mingjiedege <87758048+mingjiedege@users.noreply.github.com>
2021-07-30 21:44:24 +0800

    Add files via upload
    扫雷游戏的片段

---
+++ 扫雷.c
@@ -0,0 +1,52 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include "game.h"
+void menu()
+{
+	printf("*********************************************\n");
+	printf("***************    1.play    ****************\n");
+	printf("***************    0.exit    ****************\n");
+	printf("*********************************************\n");
+}
+void game()
+{
+	char mine[ROWS][COLS] = { 0 };
+	char show[ROWS][COLS] = { 0 };
+	//初始化
+	Initboard(mine, ROWS, COLS, '0');
+	Initboard(show, ROWS, COLS, '*');
+	//打印棋盘
+	DisplayBoard(show, ROW, COL);
+	//DisplayBoard(mine, ROW, COL);
+	//布置雷
+	SetMine(mine, ROW, COL);
+	DisplayBoard(mine, ROW, COL);
+	FindMine(mine,show, ROW, COL);
+}
+void test()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do 
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("选择错误，请重新输入！\n");
+			break;
+		}
+	} while (input);
+}
+int main()
+{
+	test();
+	return 0;
+}
